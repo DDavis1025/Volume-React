f1564ef89fd48f9425f58b81e82a3f879d2aede6
DDavis1025 <1dillondavis@gmail.com>
2020-05-23 20:46:22 -0400

    update video upload

--- src/components/add-video.js
+++ src/components/add-video.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import styled from 'styled-components';
-import { Delete } from 'styled-icons/typicons/Delete';
-import Downloaded from './downloaded';
 import VideoFields from './videoFields'
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { ButtonToggle } from "reactstrap";
 import { Container, Row, Col } from 'reactstrap';
 import axios from 'axios';
@@ -22,48 +19,9 @@ import {
 } from "react-router-dom";
 // import auth0 from 'auth0-js';
 
+
+
  
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
- 
-  return result;
-};
- 
-const grid = 3;
- 
-const getItemStyle = (isDragging, draggableStyle) => ({
-  userSelect: "none",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
- 
-  background: isDragging ? "blue" : "white",
- 
-  ...draggableStyle
-});
- 
-const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? "lightgrey" : "lightgrey",
-  padding: grid,
-  width: 700,
-});
- 
-const Handle = styled.div`
-width: 20px;
-height: 20px;
-margin-top: 7.5px;
-background-color: gray;
-border-radius: 4px;
-float: left;
-`
-const LIDelete = styled(Delete)`
-width: 20px;
-height: 20px;
-color: gray;
-float: right;
-margin-top: -50px;
-`
 
 let id = 0;
 let clicked = false;
@@ -77,13 +35,8 @@ class AddVideo extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.replaceClick = this.replaceClick.bind(this);
     // this.save = this.save.bind(this);
-    this.onDragEnd = this.onDragEnd.bind(this);
-    this.secondInputClick = this.secondInputClick.bind(this);
     this.clickFileInput = this.clickFileInput.bind(this);
     this.replaceInput = this.replaceInput.bind(this);
-    this.clickAddFiles = this.clickAddFiles.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.downloadChange = this.downloadChange.bind(this);
     // this.onSubmit = this.onSubmit.bind(this);
     this.onClick = this.onClick.bind(this);
     this.capture = this.capture.bind(this);
@@ -99,14 +52,11 @@ class AddVideo extends Component {
     this.inputRef = React.createRef();
     this.addFiles = React.createRef();
     this.resetFile = this.resetFile.bind(this);
-    this.removeItem = this.removeItem.bind(this);
 
 
  
     this.state = {
-      beforeImageSave: null,
       file: null,
-      files: [],
       isLoading: false,
       video: {},
       fileContent: null,
@@ -166,113 +116,33 @@ console.log("2");
   
 }
 
-
-
-downloadChange (event) {
-   const { name, value } = event.target;
-   this.setState(prevState => ({
-   albums: {...prevState.albums, [name]: value}
-   }));
-
-  }
-
-
- 
-changeCursor = (e) => {
-    e.target.style.background = 'red';
-  }
  
 
 handleClick = event => {
-  this.setState({ videoSelected: true })
+  this.setState({ videoSelected: true });
   if (event.target.value.length != 0) {
   this.setState({ isLoading: true });
-  console.log("it worked handle")
   }
-
-  // Helper code to read file and return promise
-  const readFile = (file) => {
  
-    // const fileList = [];
+ var file = event.target.files[0];
  
     const fileReader = new FileReader();
- 
-    // create the promise and return it
-    return new Promise((resolve, reject) => {
- 
-      // if file reader has an error, report it
+
       fileReader.onerror = (error) => {
-        reject({ error })
+        console.log(error);
       }
- 
-      // if success, resolve the promise
+
       fileReader.onload = (e) => {
-        resolve( this.setState({ video: {
-          name: file.name.replace( /_|\.mp3/gi, " "),
-          id: id++,
-         }
-        }),
-        this.setState({ fileContent: e.target.result}),
-        this.setState({ isLoading: false }),
-        console.log("FILE" + JSON.stringify(e.target.result))
-
-      )}
+        this.setState({ fileContent: e.target.result})
+        this.setState({ isLoading: false })
+        }
  
-      // start reading the file
-      fileReader.readAsDataURL(file);
- 
-    })
-  };
+      fileReader.readAsDataURL(file)
 
-  
- 
- 
-  // create all the file reader promises
-  // create an array from the files list and use map to generate
-  // an array of promises
-  const allReaders = Array.from(event.target.files).map(readFile)
-
-  const other = event.target.files;
-
-  console.log(other);
-
-  // console.log(other);
-  var promise = new Promise(function(resolve, reject) {
-  // do a thing, possibly async, then…
-
-  if (1 === 1) {
-    resolve("Stuff worked!");
-  }
-  else {
-    reject(Error("It broke"));
-  }
-});
-
-promise.then(() => {this.setState({songsList : other})
-}).then(()=> {
-console.log(this.state.songList)
- }).catch((error) => {
-  console.log(error);
- })
-   
-
- 
-  
- 
-  // Now handle the array of promises we just created
-  Promise.all(allReaders)
-    .then(fileList => {
-
-      // set the state that we have all the files
-      this.setState({ files: fileList });
-       console.log(this.state.files)
-    })
-    .catch(error => {
-       console.error(error)
-    });
-    
 }
 
+       
+  
 
 
 replaceClick = event => {
@@ -282,107 +152,24 @@ replaceClick = event => {
   }
   const canvas = this.canvasRef.current;
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-  const readFile = (file) => {
 
- 
-    // const fileList = [];
+    var file = event.target.files[0];
  
     const fileReader = new FileReader();
  
-    // create the promise and return it
-    return new Promise((resolve, reject) => {
- 
-      // if file reader has an error, report it
       fileReader.onerror = (error) => {
-        reject({ error })
+        console.log(error)
       }
  
-      // if success, resolve the promise
       fileReader.onload = (e) => {
-        resolve(this.setState({ fileContent: e.target.result}))
+        this.setState({ fileContent: e.target.result})
         this.setState({ isLoading: false })
-
       }
  
-      // start reading the file
       fileReader.readAsDataURL(file);
  
-    })
   }
 
- 
- 
-  // create all the file reader promises
-  // create an array from the files list and use map to generate
-  // an array of promises
-  const allReaders = Array.from(event.target.files).map(readFile)
-
- 
-  // Now handle the array of promises we just created
-  Promise.all(allReaders)
-    .then(fileList => {
-      console.log(this.state.files)
-      // set the state that we have all the files
-      this.setState({ files: fileList });
-    })
-    .catch(error => {
-       console.error(error)
-    });
-    
-    clicked = true;
-}
-
-
-secondInputClick = event => {
-  // Helper code to read file and return promise
-  const readFile = (file) => {
- 
-    // const fileList = [];
- 
-    const fileReader = new FileReader();
- 
-    // create the promise and return it
-    return new Promise((resolve, reject) => {
- 
-      // if file reader has an error, report it
-      fileReader.onerror = (error) => {
-        reject({ error })
-      }
- 
-      // if success, resolve the promise
-      fileReader.onload = (e) => {
-        resolve({
-          name: file.name.replace( /_|\.mp3/gi, " "),
-           id: id++,
-        })
-      }
- 
-      // start reading the file
-      fileReader.readAsDataURL(file);
- 
-    })
-  }
-
- 
- 
-  // create all the file reader promises
-  // create an array from the files list and use map to generate
-  // an array of promises
-  const allReaders = Array.from(event.target.files).map(readFile)
- 
-  // Now handle the array of promises we just created
-  Promise.all(allReaders)
-    .then(fileList => {
-      console.log(this.state.files)
-      // set the state that we have all the files
-      this.setState(prevState => ({ ...prevState, files: [...prevState.files,  ...fileList ]}));
-    })
-    .catch(error => {
-       console.error(error)
-    });
-
- 
-}
 
 
 save2(event) {
@@ -445,36 +232,8 @@ save2(event) {
 }
 
 
-  
-
-
-
-
 
  
-  onDragEnd(result) {
-    // dropped outside the list
-    if (!result.destination) {
-      return;
-    }
- 
-    const files = reorder(
-      this.state.files,
-      result.source.index,
-      result.destination.index
-    );
- 
-    this.setState({
-      files
-    });
-  }
- 
- 
-  removeItem = (index) => {
-    this.setState(({ files }) => ({
-      files: files.filter((_, ind) => ind !== index) }));
-  }
-
 
   clickFileInput() {
     this.inputRef.current.click();
@@ -488,11 +247,6 @@ save2(event) {
     this.addFiles.current.click();
   }
 
-  handleChange(index, event) {
-    let files = this.state.files.slice();
-    files[index].name = event.target.value;
-    this.setState({ files: files });
-  }
 
 onClick(event) {
   let albums = this.state.albums;
@@ -570,7 +324,7 @@ onClick(event) {
           <Row>
           <Col>
                    {this.state.isLoading &&
-                   <h4>Loading...</h4>
+                   <h4>Uploading... this may take awhile</h4>
                    }
                    <div>
                    <video src={this.state.fileContent} width="520" height="400" ref={this.videoRef} controls></video>
@@ -599,8 +353,7 @@ onClick(event) {
         </Container>
 
                   
-                      <div className="delete">
-                        </div>
+                  
 
       </div>   
                   
@@ -611,7 +364,7 @@ onClick(event) {
         <Row>
         <Col>
 
-        <div className="belowList">{this.state.files.length > 0 && <div>
+        <div className="belowList">{this.state.videoSelected && <div>
 
            <ButtonToggle onClick={this.onClick} color="success">Save
            </ButtonToggle> 
